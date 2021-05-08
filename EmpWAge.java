c71efeb3bb040b11dcc4be7e86c99a02068dfc39
unknown <ankitghorpade59@gmail.com>
2021-05-08 08:07:11 -0800

    refactor the code

--- EmpWAge.java
+++ EmpWAge.java
@@ -6,9 +6,9 @@ public class EmpWAge {
 	public static final	int IS_FULL_TIME = 2;
 	public static final	int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 2;
-	public static final int MAX_HRS_IN_MONTH = 10;
+	public static final int MAX_HRS_IN_MONTH = 100;
 	
-	public static void main(String[] args) {
+	public static int computeEmpWage() {
 		//Variable
 		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays= 0;
 		//Computation
@@ -31,5 +31,9 @@ public class EmpWAge {
 		}
 		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Total Emp Wage:" + totalEmpWage);
+		return totalEmpWage;
 	 }
+	public static void main(String[] args) {
+		computeEmpWage();
+	}
 	}
