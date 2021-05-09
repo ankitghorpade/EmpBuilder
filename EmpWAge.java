9cf08e185690cd6df6b3794ba7d19ad64a169569
ankitghorpade <75006687+ankitghorpade@users.noreply.github.com>
2021-05-09 07:03:41 -0800

    Update EmpWAge.java

--- EmpWAge.java
+++ EmpWAge.java
@@ -6,7 +6,7 @@ public class EmpWAge {
 	public static final	int IS_FULL_TIME = 2;
 	public static final	int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 2;
-	public static final int MAX_HRS_IN_MONTH = 100;
+	public static final int MAX_HRS_IN_MONTH = 10;
 	
 	public static int computeEmpWage() {
 		//Variable
